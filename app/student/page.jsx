4e2b52cfb686c2debc75419d202459270ce4ef3e
Prz3msik0 <przemozagaja@gmail.com>
2024-09-28 17:30:47 +0200

    gift

--- app/student/page.jsx
+++ app/student/page.jsx
@@ -2,7 +2,9 @@ import StudentSidebar from "../components/StudentSidebar";
 import Link from "next/link";
 import Image from "next/image";
 import { MyButton } from "../components/buttons";
-import {RoundedButton} from "../components/buttons";
+import { RoundedButton } from "../components/buttons";
+import { motion } from "framer-motion";
+import { LearnItems } from "@/app/constants/index";
 
 export default function Learn() {
 
@@ -24,10 +26,22 @@ export default function Learn() {
                     </div>
                 </div>
             </nav>
-            <div className="flex flex-col mt-[14rem]">
-                <RoundedButton className="flex text-black rounded-full">
-                    1
-                </RoundedButton>
+            <div className="flex flex-col mt-[12rem]">
+
+                <h1 className="text-3xl text-gray-300 font-bold py-4 ml-10 ">Dział 1: Budowa maszyn CNC:</h1>
+                <div className="flex flex-row gap-x-3 ml-10 items-center">
+                    {LearnItems.map((item, index) => (
+                        <Link className="flex text-green-900 bg-green-400 rounded-full px-7 py-5 border-green-900 text-xl font-extrabold border-3 active:scale-90" href={item.href}
+                            key={index}>{item.title}
+                        </Link>
+                    )
+                    )}
+
+                    <MyButton className="text-gray-500">
+                        Odbierz nagrodę
+                        <Image src="/images/present.gif" alt="nagroda" width={60} height={60} />
+                    </MyButton>
+                </div>
             </div>
         </div>
     );
